6882385cf299ec97252218d3cd95915d789b8288
Blair Bristow <blairhbristow@gmail.com>
2022-05-17 13:29:13 -0400

    Create three components git push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NightLightComponent } from './components/night-light/night-light.component';
+import { LightComponent } from './components/light/light.component';
+import { SwitchComponent } from './components/switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NightLightComponent,
+    LightComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule
